3bc1eca3d1871077ca77eee45081d95b04dfd8a3
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-01-30 21:57:43 +0300

    feat: complete js validation to form

--- index.js
+++ index.js
@@ -49,19 +49,146 @@ window.addEventListener("resize", () => {
   }
 });
 
-//form
-//https://fakestoreapi.com/docs
+class FormsValidation {
+  selectors = {
+    form: "[data-js-form]",
+    fieldErrors: "[data-field-errors]",
+    fieldSuccess: "[data-field-success]",
+  };
 
-const form = document.querySelector(".footer__form");
-if (form) {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    fetch("https://fakestoreapi.com/products", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  });
+  errorMessages = {
+    valueMissing: () => "Пожалуйста, заполните это поле",
+    patternMismatch: ({ title }) => title || "Данные не соответствуют формату",
+  };
+
+  constructor() {
+    this.bindEvents();
+  }
+
+  manageErrors(fieldControlElement, errorMessages) {
+    const fieldErrorsElement = fieldControlElement.parentElement.querySelector(
+      this.selectors.fieldErrors
+    );
+
+    fieldErrorsElement.innerHTML = errorMessages
+      .map((message) => `<span class="field__error">${message}</span>`)
+      .join("");
+  }
+
+  manageSuccess() {
+    const fieldSuccessElement = document.querySelector(
+      this.selectors.fieldSuccess
+    );
+
+    fieldSuccessElement.textContent = "Cообщение успешно отправлено";
+    setTimeout(() => {
+      fieldSuccessElement.textContent = "";
+    }, 3000);
+  }
+
+  validateField(fieldControlElement) {
+    const errors = fieldControlElement.validity;
+    const errorMessages = [];
+
+    Object.entries(this.errorMessages).forEach(
+      ([errorType, getErrorMessage]) => {
+        if (errors[errorType]) {
+          errorMessages.push(getErrorMessage(fieldControlElement));
+        }
+      }
+    );
+
+    this.manageErrors(fieldControlElement, errorMessages);
+
+    const isValid = errorMessages.length === 0;
+
+    fieldControlElement.ariaInvalid = !isValid;
+
+    return isValid;
+  }
+
+  onBlur(event) {
+    const { target } = event;
+    const isFormField = target.closest(this.selectors.form);
+    const isRequired = target.required;
+
+    if (isFormField && isRequired) {
+      this.validateField(target);
+    }
+  }
+
+  onSubmit(event) {
+    const isFormElement = event.target.matches(this.selectors.form);
+    if (!isFormElement) {
+      return;
+    }
+
+    const requiredControlElements = [...event.target.elements].filter(
+      ({ required }) => required
+    );
+    let isFormValid = true;
+    let firstInvalidFieldControl = null;
+
+    requiredControlElements.forEach((element) => {
+      const isFieldValid = this.validateField(element);
+
+      if (!isFieldValid) {
+        isFormValid = false;
+
+        if (!firstInvalidFieldControl) {
+          firstInvalidFieldControl = element;
+        }
+      }
+    });
+
+    if (!isFormValid) {
+      event.preventDefault();
+      firstInvalidFieldControl.focus();
+    } else {
+      event.preventDefault();
+
+      const form = document.querySelector([this.selectors.form]);
+
+      const formData = new FormData(form);
+      fetch("https://fakestoreapi.com/products", {
+        method: "POST",
+        body: formData,
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        } else {
+          this.setClearInputs(form);
+          this.manageSuccess();
+          res.json().then((json) => console.log(json));
+        }
+      });
+    }
+  }
+  onChange(event) {
+    const { target } = event;
+    const isRequired = target.required;
+    if (isRequired) {
+      this.validateField(target);
+    }
+  }
+
+  setClearInputs(form) {
+    [...form.elements].forEach((element) => {
+      element.value = "";
+    });
+  }
+
+  bindEvents() {
+    document.addEventListener(
+      "blur",
+      (event) => {
+        this.onBlur(event);
+      },
+      { capture: true }
+    );
+    document.addEventListener("change", (event) => this.onChange(event));
+    document.addEventListener("submit", (event) => this.onSubmit(event));
+  }
 }
+
+new FormsValidation();
